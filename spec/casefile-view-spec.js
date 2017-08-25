83b1ef0c93ea38b8006027fda4d0f97d83d8b33f
Richard Weeks <rtweeks21@gmail.com>
2017-08-25 12:13:57 -0700

    Implement a basic dialog for bookmark addition

---
+++ spec/casefile-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CasefileView from '../lib/casefile-view';
+
+describe('CasefileView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
